00e47cc4ee495236d083d4019ac8b360c258e29d
Boris Tane <boris.tane@gmail.com>
2019-05-24 11:47:26 +0100

    Fix bytes listing

--- src/controlers/bytes.ts
+++ src/controlers/bytes.ts
@@ -13,17 +13,33 @@ export async function getMany(req: Request, res: Response): Promise<Response> {
     const numPerPage = 10;
     const byteRepo = getRepository(Byte);
 
-    const bytes = await byteRepo
+    const result = await byteRepo
       .createQueryBuilder("byte")
+      .leftJoinAndSelect("byte.author", "author")
       .select()
-      .orderBy("byte.created", "ASC")
+      .orderBy("byte.created", "DESC")
       .skip((page - 1) * numPerPage)
       .take(numPerPage)
       .getMany();
 
-    if (bytes.length === 0) {
+    if (result.length === 0) {
       return send404(res);
     }
+
+    const bytes = result.map(byte => {
+      return {
+        id: byte.id,
+        title: byte.title,
+        created: byte.created,
+        updated: byte.updated,
+        image: byte.image,
+        body: byte.body,
+        tags: byte.tags,
+        author: {
+          name: byte.author.name
+        }
+      };
+    });
 
     return res.status(200).send({ bytes, page, count: bytes.length });
   } catch (err) {
