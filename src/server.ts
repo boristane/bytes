c650d357d953b4716d1dbb16d8ca91904d345bfc
Boris Tane <boris.tane@gmail.com>
2019-06-02 18:39:15 +0100

    Allows CORS

--- src/server.ts
+++ src/server.ts
@@ -6,5 +6,18 @@ require("dotenv").config();
 export const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  return next();
+});
+
 app.use("/user/", usersRouter);
 app.use("/byte/", bytesRouter);
