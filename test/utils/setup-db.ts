4a9124b6221a8947182370c5fde7b7accbd610fa
Boris Tane <boris.tane@gmail.com>
2020-01-12 15:55:03 +0000

    dont populate the db on initialisation

--- test/utils/setup-db.ts
+++ test/utils/setup-db.ts
@@ -38,7 +38,7 @@ export default async function main() {
   const connection = await createConnectionToDB();
   await connection.synchronize(true);
   console.log("Database initialised.");
-  await populateDB();
-  console.log("Database populated.");
+  // await populateDB();
+  // console.log("Database populated.");
   await connection.close();
 }
